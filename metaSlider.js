53e50d4771268f8b9e172e548ff811f6814a5b1f
cdnCreater <87004891+arsl251@users.noreply.github.com>
2021-10-05 21:29:51 +0500

    Update metaSlider.js

--- metaSlider.js
+++ metaSlider.js
@@ -1,6 +1,1 @@
-        //let now = new Date;
-        //let next = new Date("2021/12/08");
-        //let ownVal = (next - now) / (1000 * 60 * 60 * 24);
-        //ownVal = Math.floor(ownVal);
-        //let ownF = ownVal * (0.033);
-        //document.querySelector('*').style.opacity = ownF;
+
